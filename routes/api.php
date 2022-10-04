cf7a100348a622784fe29dcfe02838bb769f2e94
MrAndrewsHere <andrews.mr@yandex.ru>
2022-10-04 16:36:25 +0300

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Domain\Controllers\AdController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::prefix('/v1')->group(function () {
+    Route::get('/', fn() => app()->version());
+
+    Route::get('/ads', [AdController::class, 'index']);
+    Route::get('/ad', [AdController::class, 'get']);
+    Route::post('/ad', [AdController::class, 'store']);
+});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
